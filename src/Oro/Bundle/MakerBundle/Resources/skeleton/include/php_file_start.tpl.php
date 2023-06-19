d43159a276e1bf032cb2dc924de65ec0e92ac23c
Andrii Yatsenko <ayatsenko@oroinc.com>
2023-06-19 11:14:07 +0300

    BAP-21019: Switch to PSR-12 coding standard except explicit constants visibility requirement (#35870)
    - used PSR-12 except required visibility for constants
    - updated php-cs-fixer config
    - added doc block comments for changed classes
    - updated php-semver-checker

--- src/Oro/Bundle/MakerBundle/Resources/skeleton/include/php_file_start.tpl.php
+++ src/Oro/Bundle/MakerBundle/Resources/skeleton/include/php_file_start.tpl.php
@@ -2,5 +2,6 @@
 
 namespace <?= $namespace ?>;
 
-<?php $uses = array_merge($uses ?? [], $traits ?? [], $interfaces ?? []);sort($uses); ?>
+<?php $uses = array_merge($uses ?? [], $traits ?? [], $interfaces ?? []);
+sort($uses); ?>
 <?= implode('' . PHP_EOL, array_map(fn ($use) => 'use ' . $use . ';', $uses)) . PHP_EOL ?>
