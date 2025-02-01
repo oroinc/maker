09da103047c8556e5cff013dc5c4d17ddd3a172c
Vova Soroka <vsoroka@oroinc.com>
2025-02-01 15:19:43 +0200

    BAP-22921: Fix code styles for method parameters (#40353)

--- src/Oro/Bundle/MakerBundle/Generator/Generator.php
+++ src/Oro/Bundle/MakerBundle/Generator/Generator.php
@@ -18,9 +18,9 @@ class Generator extends BaseGenerator
     private GeneratorTwigHelper $twigHelper;
 
     public function __construct(
-        FileManager                 $fileManager,
-        string                      $namespacePrefix,
-        ?PhpCompatUtil              $phpCompatUtil = null,
+        FileManager $fileManager,
+        string $namespacePrefix,
+        ?PhpCompatUtil $phpCompatUtil = null,
         ?TemplateComponentGenerator $templateComponentGenerator = null
     ) {
         $this->fileManager = $fileManager;
